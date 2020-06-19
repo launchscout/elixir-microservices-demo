240770c80ba454c2461ded7518e8c04038f76020
Stamates <Stamates@users.noreply.github.com>
2020-06-18 21:56:26 -0400

    Fix config imports in product_server

--- product_server/config/config.exs
+++ product_server/config/config.exs
@@ -1,4 +1,4 @@
-import Config
+use Mix.Config
 
 config :product_server,
   ecto_repos: [ProductServer.Repo]
--- product_server/config/test.exs
+++ product_server/config/test.exs
@@ -1,4 +1,4 @@
-import Config
+use Mix.Config
 
 config :product_server, ProductServer.Repo,
   username: "postgres",
