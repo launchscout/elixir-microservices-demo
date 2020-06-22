b6db929dd1739bfb908ff4432582f8dcb913df78
Stamates <Stamates@users.noreply.github.com>
2020-06-22 09:06:36 -0400

    Move local cluster startup to test_helper (sets state data in fake GenServers).

--- web_server/test/test_helper.exs
+++ web_server/test/test_helper.exs
@@ -1,3 +1,10 @@
+:ok = WebServer.LocalCluster.start(:web_server)
+
+WebServer.LocalCluster.start_nodes([:auth_server, :product_server],
+  auth_server: {:ok, %{email: "account@email.com"}},
+  product_server: %{}
+)
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(WebServer.Repo, :manual)
 {:ok, _} = Application.ensure_all_started(:wallaby)
--- web_server/test/web_server/accounts_test.exs
+++ web_server/test/web_server/accounts_test.exs
@@ -1,49 +1,10 @@
 defmodule WebServer.AccountsTest do
   use WebServer.DataCase
-  alias WebServer.{Accounts, LocalCluster}
-  @response {:ok, %{email: "account@email.com"}}
+  alias WebServer.Accounts
 
-  test "spawning tasks on a cluster" do
-    :ok = LocalCluster.start(:web_server)
-
-    [auth_server, product_server] =
-      LocalCluster.start_nodes([:auth_server, :product_server],
-        auth_server: @response,
-        product_server: %{}
-      )
-
-    assert Node.ping(auth_server) == :pong
-    assert Node.ping(product_server) == :pong
-
-    caller = self()
-
-    # WebServer.AuthServerFake.start_link(@response) |> IO.inspect()
-
-    # Node.spawn(auth_server, WebServer.AuthServerFake, :start_link, [@response])
-
-    # Node.spawn(auth_server, fn ->
-    #   DynamicSupervisor.start_child(
-    #     WebServer.DynamicSupervisor,
-    #     {WebServer.AuthServerFake, fn -> @response end}
-    #   )
-    # end)
-
-    #
-    # Node.spawn(product_server, fn ->
-    #   send(caller, :from_node_2)
-    # end)
-    #
-    # assert_receive :from_node_1
-    # assert_receive :from_node_2
+  test "regitster/1" do
     assert {:ok, %{email: "account@email.com"}} = Accounts.register(valid_account_params())
   end
-
-  # test "register for an account with valid information" do
-  #   IO.inspect(:global.registered_names())
-  #   params = valid_account_params()
-  #
-  #   assert {:ok, %{email: "account@email.com"}} = Accounts.register(params)
-  # end
 
   defp valid_account_params do
     %Ueberauth.Auth{
@@ -54,7 +15,7 @@ defmodule WebServer.AccountsTest do
         }
       },
       info: %Ueberauth.Auth.Info{
-        email: "me@example.com"
+        email: "account@email.com"
       }
     }
   end
